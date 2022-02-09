d687e5992baba8c07664ad9a2c42ede8c1d5fdc4
Maria-C1 <mariacrawford789@gmail.com>
2022-02-09 14:24:38 +0000

    grid appearing!

--- noughts-and-crosses/src/App.js
+++ noughts-and-crosses/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Board from './components/board';
 import Game from './components/game';
 function App() {
 	return (
 		<div className="App">
-			<Game /> App Page
+			<Board /> App Page
 		</div>
 	);
 }
--- noughts-and-crosses/src/components/game/index.js
+++ noughts-and-crosses/src/components/game/index.js
@@ -1,20 +1,21 @@
-import { useState } from 'react';
-import Board from '../board';
+import { useState } from "react";
+import Board from "../board";
 
 function Game() {
-	const [ move, setMove ] = useState([]);
-	const [ turn, setTurn ] = useState(false);
-	function handleTurn() {
-		setTurn(!turn);
-		// triggered by start button, and when a player has taken their go.
-	}
+  const [ move, setMove ] = useState([]);
+  const [ turn, setTurn ] = useState(false);
+  function HandleTurn() {
+  	setTurn(!turn);
+  // triggered by start button, and when a player has taken their go.
+  }
 
-	return (
-		<div>
-			Hello
-			<Board handleTurn={handleTurn} move={move} />
-		</div>
-	);
+  return (
+    <div>
+      Hello
+      <Board HandleTurn={HandleTurn} move={move}/>
+    </div>
+  );
 }
 
 export default Game;
+
